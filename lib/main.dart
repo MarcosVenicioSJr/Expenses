9607890b7df2c4ffc6d41d14bf6344f0bcbb9ad0
Marcos Venicio <marcos.venicio1230@hotmail.com>
2025-07-21 20:37:17 -0300

    Add Style aos Widgets e instalando pacote intl

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() {
   runApp(const ExpensesApp());
@@ -16,8 +17,18 @@ class ExpensesApp extends StatelessWidget {
 
 class MyHome extends StatelessWidget {
   final _transactions = [
-    Transaction(id: "t1", title: "Tenis de Corrida", value: 310.00, date: DateTime.now()),
-    Transaction(id: "t2", title: "Notebook", value: 1300.00, date: DateTime.now())
+    Transaction(
+      id: "t1",
+      title: "Tenis de Corrida",
+      value: 310.00,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Notebook",
+      value: 1300.00,
+      date: DateTime.now(),
+    ),
   ];
 
   @override
@@ -43,12 +54,52 @@ class MyHome extends StatelessWidget {
           Column(
             children: _transactions.map((tr) {
               return Card(
-                child: Text(tr.title),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 2),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        'R\$ ${tr.value.toStringAsFixed(2)}',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          tr.title,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          DateFormat('d MMM y').format(tr.date),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               );
             }).toList(),
-          )
-        ]
-      )
+          ),
+        ],
+      ),
     );
   }
 }
